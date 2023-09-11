1c81997245d65ae882cfa973e5dceb6df3385167
ergincetn01 <ergincetn01@gmail.com>
2023-09-11 11:45:55 +0300

    welcome screen impl.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_hive_todoapp/screens/welcome_screen.dart';
 
 class GetxHiveTodoApp extends StatelessWidget {
   const GetxHiveTodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const WelcomeScreen(),
+    );
   }
 }
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_hive/widgets/button.dart';
+
+import 'add_todo_screen.dart';
+import 'todo_list_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 150,
+            ),
+            Center(
+              child: Text(
+                "Welcome to",
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text(
+                "Todoey",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(
+                horizontal: 10,
+              ),
+              child: Text(
+                "My Todoey hepls you stay organized and perform your tasks much faster.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            PrimaryButton(
+              height: 50.0,
+              onPressed: () {
+                Get.to(() => TodosScreen());
+              },
+              color: Colors.blue,
+              title: 'Add Todo',
+              icon: Icons.add,
+            ),
+            SizedBox(height: 20),
+            PrimaryButton(
+              height: 50.0,
+              onPressed: () {
+                Get.to(() => MyTodosScreen());
+              },
+              title: 'My Todos',
+              icon: Icons.task,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
